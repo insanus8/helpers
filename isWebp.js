b94bb9b88eec3a22f2f2997a95b25136f956f977
insanus8 <67479442+insanus8@users.noreply.github.com>
2022-02-24 02:08:01 +0500

    browser support check function webp

---
+++ isWebp.js
@@ -0,0 +1,7 @@
+export function isWebp() {
+    const webP = new Image();
+        webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+        webP.onload = webP.onerror = function () {
+          webP.height === 2 ? document.body.classList.add('webp') : document.body.classList.add('no-webp')
+    }
+}
